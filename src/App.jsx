278ab6766f1d5892cb9ed7102d27adb46f6bee35
sergio <kalero_008@hotmail.com>
2024-02-06 10:23:45 +0100

    intento2

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './App.css'
+
+// Función Flecha o Arrow Function
+const App = () => {
+    // Lo que ejecuta la función
+    console.log("Renderización de App")
+    return (
+    <main className='react-calculator'>
+        <div className="result">
+        </div>
+        <div className="numbers">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+        </div>
+        <div className="functions">
+            <button>
+                clear
+            </button>
+            <button>
+                r
+            </button>
+        </div>
+        <div className="math-operations">
+            <button>+</button>
+            <button>-</button>
+            <button>*</button>
+            <button>/</button>
+            <button>=</button>
+        </div>
+    </main>)
+}
+
+export default App
